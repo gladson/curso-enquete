789365a8a11f64f0bb855fd68a56e4147edf1443
ubuntu-server <gladson@agronomo.eng.br>
2012-10-19 14:34:04 -0700

    alterando - theme - bootstrap

--- source/conf.py
+++ source/conf.py
@@ -91,7 +91,11 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+#html_theme = 'default'
+
+sys.path.insert(0, os.path.abspath('.'))
+html_theme_path = ['_themes']
+html_theme = 'sphinx-bootstrap'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
